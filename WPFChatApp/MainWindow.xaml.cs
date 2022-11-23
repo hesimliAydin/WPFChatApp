4330413a620ef57422b1cfad2a7b9cb42b5284e5
ZoreX <aydinhsimli@gmail.com>
2022-11-24 01:33:51 +0400

    Add project files.

---
+++ WPFChatApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFChatApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseDown1(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseDown2(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseDown3(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseDown4(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Grid_MouseDown5(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
