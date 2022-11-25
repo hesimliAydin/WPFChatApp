75f1380005697e932603b496336f4cc85701aca7
ZoreX <aydinhsimli@gmail.com>
2022-11-26 03:48:14 +0400

    done

---
+++ WPFChatApp/Chat.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFChatApp
+{
+    /// <summary>
+    /// Interaction logic for Chat.xaml
+    /// </summary>
+    public partial class Chat : UserControl
+    {
+        public Chat()
+        {
+            InitializeComponent();
+        }
+
+        private void rightMessageTextBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (rightMessageTextBox.Text.ToString() == "Type Something")
+                rightMessageTextBox.Text = String.Empty;
+        }
+
+
+        private void rightMessageTextBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (rightMessageTextBox.Text.ToString() == String.Empty)
+                rightMessageTextBox.Text = "Type Something";
+        }
+
+        bool isCheck = false;
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isCheck == false)
+            {
+
+                var message = new Chat();
+                message.HorizontalAlignment = HorizontalAlignment.Right;
+                message.Content = rightMessageTextBox.Text.ToString();
+                message.Height = 30;
+                ChatGrid.Children.Add(message);
+                rightMessageTextBox.Text = String.Empty;
+                isCheck = true;
+            }
+            else
+            {
+                var message = new Chat();
+                message.HorizontalAlignment = HorizontalAlignment.Left;
+                message.Content = rightMessageTextBox.Text.ToString();
+                message.Height = 30;
+                ChatGrid.Children.Add(message);
+                rightMessageTextBox.Text = String.Empty;
+                isCheck = false;
+            }
+
+        }
+    }
+}
--- WPFChatApp/MainWindow.xaml.cs
+++ WPFChatApp/MainWindow.xaml.cs
@@ -27,32 +27,67 @@ namespace WPFChatApp
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
 
+            uc1.Width = 640;
+            uc1.Message1Lbl.Content = "Salam aleykum";
+            
+            MessageGrid.Children.Add(uc1);
         }
 
         private void Grid_MouseDown1(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
+            uc1.NameCentral.Content = "Kamran";
+            uc1.Width = 640;
+            uc1.ImageCentral.Source = new BitmapImage(new Uri(@"/Screenshot_2.png", UriKind.Relative));
+            uc1.Message1Lbl.Content = "Salam aleykum";
 
+            MessageGrid.Children.Add(uc1);
         }
 
         private void Grid_MouseDown2(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
+            uc1.ImageCentral.Source = new BitmapImage(new Uri(@"/Screenshot_3.png", UriKind.Relative));
+            uc1.NameCentral.Content = "Huseyn";
+            uc1.Width = 640;
+            uc1.Message1Lbl.Content = "Salam aleykum";
 
+            MessageGrid.Children.Add(uc1);
         }
 
         private void Grid_MouseDown3(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
+            uc1.ImageCentral.Source = new BitmapImage(new Uri(@"/Screenshot_4.png", UriKind.Relative));
+            uc1.NameCentral.Content = "Fatih";
+            uc1.Width = 640;
+            uc1.Message1Lbl.Content = "Salam aleykum";
 
+            MessageGrid.Children.Add(uc1);
         }
 
         private void Grid_MouseDown4(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
+            uc1.ImageCentral.Source = new BitmapImage(new Uri(@"/Screenshot_5.png", UriKind.Relative));
+            uc1.NameCentral.Content = "Omer";
+            uc1.Width = 640;
+            uc1.Message1Lbl.Content = "Salam aleykum";
 
+            MessageGrid.Children.Add(uc1);
         }
 
         private void Grid_MouseDown5(object sender, MouseButtonEventArgs e)
         {
+            Chat uc1 = new Chat();
+            uc1.ImageCentral.Source = new BitmapImage(new Uri(@"/Screenshot_6.png", UriKind.Relative));
+            uc1.NameCentral.Content = "Sari";
+            uc1.Width = 640;
+            uc1.Message1Lbl.Content = "Salam aleykum";
 
+            MessageGrid.Children.Add(uc1);
         }
     }
 }
